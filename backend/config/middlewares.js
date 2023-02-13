89d6df09f636a74f6c4e6649737ff782e5fd4267
Kawara <kawara@Kawaras-MacBook-Pro.local>
2023-02-14 00:18:10 +0300

    Changed the store image to cover.

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  // 'strapi::body',
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "50mb", // modify form body
+      jsonLimit: "50mb", // modify JSON body
+      textLimit: "50mb", // modify text body
+      formidable: {
+        maxFileSize: 50 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
 ];
---
+++ backend/config/plugins.js
@@ -0,0 +1,7 @@
+module.exports = {
+  upload: {
+    config: {
+      sizeLimit: 50 * 1024 * 1024, // 256mb in bytes
+    },
+  },
+};
