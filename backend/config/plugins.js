b6b3bcd847f96da035950bf2e1ab4b82d9036da5
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-03-08 00:04:36 +0300

    Backend, Added mailing provider.

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,7 +1,20 @@
-module.exports = {
+module.exports = ({ env }) => ({
   upload: {
     config: {
       sizeLimit: 50 * 1024 * 1024, // 256mb in bytes
     },
   },
-};
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        service: "gmail",
+        auth: { user: env("NODEMAILER_USER"), pass: env("NODEMAILER_PASS") },
+      },
+      settings: {
+        defaultFrom: "noreply@recipee.com",
+        defaultReplyTo: "noreply@recipee.com",
+      },
+    },
+  },
+});
