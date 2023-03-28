0f763c8d2bf4e7c200fb82d8533a4bc3cf426a2d
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-03-28 22:57:44 +0300

    Moved to sqlite.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT", 3306),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      database: env("DATABASE_NAME"),
-      ssl: env.bool("DATABASE_SSL"),
-    },
+    client: "sqlite",
+    connection: { filename: process.cwd() + env("SQLITE_DATABASE_FILENAME") },
+    useNullAsDefault: true,
+
+    // client: "mysql",
+    // connection: {
+    //   host: env("DATABASE_HOST"),
+    //   port: env.int("DATABASE_PORT", 3306),
+    //   user: env("DATABASE_USERNAME"),
+    //   password: env("DATABASE_PASSWORD"),
+    //   database: env("DATABASE_NAME"),
+    //   ssl: env.bool("DATABASE_SSL"),
+    // },
   },
 });
