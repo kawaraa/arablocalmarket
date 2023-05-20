5f18b889c8d1ac21e4a50d2e7670fe04639ee7b5
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-05-20 12:22:47 +0300

    Add extra sucriry to external links.

--- backend/src/extensions/users-permissions/content-types/user/strapi-server.js
+++ backend/src/extensions/users-permissions/content-types/user/strapi-server.js
@@ -3,42 +3,42 @@
 // https://forum.strapi.io/t/users-me-update/15187
 // https://www.youtube.com/watch?v=2ZwiiY6tnmw
 
-// module.exports = (plugin) => {
-//   /*******************************  CUSTOM CONTROLERS  ********************************/
+module.exports = (plugin) => {
+  //   /*******************************  CUSTOM CONTROLERS  ********************************/
+  console.log(plugin.controllers.user);
+  plugin.controllers.user.update = async (ctx) => {
+    console.log("AAAA");
+    // return {};
+  };
 
-//   plugin.controllers.user.update = async (ctx) => {
-//     console.log("AAAA");
-//     // return {};
-//   };
+  //   plugin.controllers.user.updateMe = async (ctx) => {
+  //     console.log("User: >>> ", ctx.state.user);
+  //     console.log("Data: >>> ", ctx.request.body);
+  //     if (!ctx.state.user || ctx.state.user.id) {
+  //       // ctx.badRequest('forbidden to update other user')
+  //       return (ctx.response.status = 403);
+  //     }
+  //     await strapi
+  //       .query("plugin::users-permissions.user")
+  //       .update({
+  //         where: { id: ctx.state.user.id },
+  //         data: ctx.request.body,
+  //       })
+  //       .then((res) => {
+  //         ctx.response.status = 200;
+  //       });
+  //   };
 
-//   //   plugin.controllers.user.updateMe = async (ctx) => {
-//   //     console.log("User: >>> ", ctx.state.user);
-//   //     console.log("Data: >>> ", ctx.request.body);
-//   //     if (!ctx.state.user || ctx.state.user.id) {
-//   //       // ctx.badRequest('forbidden to update other user')
-//   //       return (ctx.response.status = 403);
-//   //     }
-//   //     await strapi
-//   //       .query("plugin::users-permissions.user")
-//   //       .update({
-//   //         where: { id: ctx.state.user.id },
-//   //         data: ctx.request.body,
-//   //       })
-//   //       .then((res) => {
-//   //         ctx.response.status = 200;
-//   //       });
-//   //   };
+  //   /*******************************  CUSTOM ROUTES  ********************************/
+  //   //   plugin.routes["content-api"].routes.push({
+  //   //     method: "PUT",
+  //   //     path: "/users/me",
+  //   //     handler: "user.updateMe",
+  //   //     config: {
+  //   //       prefix: "",
+  //   //       policies: [],
+  //   //     },
+  //   //   });
 
-//   /*******************************  CUSTOM ROUTES  ********************************/
-//   //   plugin.routes["content-api"].routes.push({
-//   //     method: "PUT",
-//   //     path: "/users/me",
-//   //     handler: "user.updateMe",
-//   //     config: {
-//   //       prefix: "",
-//   //       policies: [],
-//   //     },
-//   //   });
-
-//   return plugin;
-// };
+  return plugin;
+};
