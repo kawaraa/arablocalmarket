a8d0561dd125557cd3f83500d611f5ede6080260
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-06-07 14:43:11 +0300

    Fixing reloading issues v-3

--- frontend/app/(component)/select-language.js
+++ frontend/app/(component)/select-language.js
@@ -1,14 +1,14 @@
 "use client";
 import { useContext, useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { AppSessionContext } from "../app-session-context";
 import Modal from "./(styled)/modal";
 import { CheckCard } from "./(styled)/inputs";
 import { Cookies } from "../(service)/utilities";
 
 export default function SelectLanguage({ serverLang }) {
-  const router = useRouter();
-  const { lang, user, updateLang } = useContext(AppSessionContext);
+  // const router = useRouter();
+  const { lang, user, setAppLoading, updateLang } = useContext(AppSessionContext);
   const [open, setOpen] = useState(false);
 
   const changeLanguage = (lang) => {
@@ -20,8 +20,10 @@ export default function SelectLanguage({ serverLang }) {
     const clientLang = Cookies.get("lang") || window.localStorage.getItem("lang");
     if (!clientLang) setTimeout(() => setOpen(true), 300);
     else if (clientLang && clientLang != serverLang && !user?.loading) {
-      router.refresh();
-      Cookies.set("lang", lang);
+      setAppLoading(true);
+      // Cookies.set("lang", lang);
+      // router.refresh();
+      setTimeout(() => window.location.reload(), 500);
       // window.location.reload();
     }
   }, [lang, serverLang, user]);
