0e58608dd12e6ac9d1f474f1871f1298b5b2b3e7
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-06-20 18:42:42 +0300

    Changed the logo colors.

--- frontend/app/signin/page.js
+++ frontend/app/signin/page.js
@@ -58,7 +58,7 @@ export default function SignIn() {
             alt="Arab Local market Logo"
             width="100"
             height="100"
-            className="h-auto w-16 mx-auto"
+            className="h-auto w-24 mx-auto"
           />
 
           <h1 className="mt-6 text-center text-2xl font-bold ">{content.h1[lang]}</h1>
