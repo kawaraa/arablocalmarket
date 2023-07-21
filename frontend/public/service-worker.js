e8bf84d13af872a403de9e2a4c245f299760588d
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-07-21 13:34:06 +0300

    Fixing sitemap.xml file.

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,6 @@
 // self.importScripts('foo.js', 'bar.js');
 
-const staticFileCacheName = "static-files-v-8987";
+const staticFileCacheName = "static-files-v-898";
 // const filesMustCache = /(googleapis|gstatic)|\.(JS|CSS|SVG|PNG|JPG|jPEG|GIF|ICO|JSON)$/gim;
 const staticFileCachePaths = [
   "/",
