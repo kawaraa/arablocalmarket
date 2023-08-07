6e64968931b2473242ee1afac962fa0ff1dcb616
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-08-07 10:43:23 +0300

    Implementing offline mode.

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,6 @@
 // self.importScripts('foo.js', 'bar.js');
 
-const staticFileCacheName = "static-files-v-1";
+const staticFileCacheName = "static-files-v-0";
 // const filesMustCache = /(googleapis|gstatic)|\.(JS|CSS|SVG|PNG|JPG|jPEG|GIF|ICO|JSON)$/gim;
 const staticFileCachePaths = ["/", "/offline.html", "/barcode-scanner/quagga.min.js", "/signin", "/signup"];
 
@@ -31,17 +31,29 @@ const handleRequest = async (request) => {
       return await fetch(request);
     } else {
       const cachedResponse = await caches.match(request);
+      console.log(
+        "cachedResponse: >>> ",
+        navigator.onLine,
+        request.method,
+        request.url,
+        " >>> ",
+        cachedResponse
+      );
+
       if (cachedResponse) return cachedResponse;
       // console.log("Caching: >>> ", navigator.onLine, request.method, request.url);
 
       const response = await fetch(request);
-      if (request.method != "GET" || !response.ok) return response;
+      if (request.method == "GET" || !response.ok) {
+        console.log("response: >>> ", response);
+        return response;
+      }
 
       await caches.open(staticFileCacheName).then((cache) => cache.put(request, response.clone()));
       return response;
     }
   } catch (error) {
-    // console.log("caches ERROR: >>>", request.method, request.url, error);
+    console.log("caches ERROR: >>>", request.method, request.url, error);
     if (request.url.includes("_next")) return caches.match(staticFileCachePaths[1]); // offline fallback page
     return networkErrorResponse;
   }
