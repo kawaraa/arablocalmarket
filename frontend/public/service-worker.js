6b81793d9fb12933c8a345d98232f5a71b78a684
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-08-09 09:46:48 +0300

    Fixing language change issue v-2.

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,6 @@
 // self.importScripts('foo.js', 'bar.js');
 
-const staticFileCacheName = "static-files-v-09y655e794498568";
+const staticFileCacheName = "static-files-v-09y655e7944985683";
 // const filesMustCache = /(googleapis|gstatic)|\.(JS|CSS|SVG|PNG|JPG|jPEG|GIF|ICO|JSON)$/gim;
 const staticFileCachePaths = ["/", "/offline.html", "/barcode-scanner/quagga.min.js", "/signin", "/signup"];
 
@@ -27,7 +27,7 @@ const handleRequest = async (request) => {
     // console.log("Caching: >>> ", navigator.onLine, request.method, request.url);
     if (/api|api\/auth|api\/users/gim.test(request.url)) return await fetch(request);
     else {
-      const cachedResponse = await caches.match(request);
+      const cachedResponse = await caches.match(request, { ignoreVary: true });
       if (cachedResponse) return cachedResponse;
 
       const response = await fetch(request);
