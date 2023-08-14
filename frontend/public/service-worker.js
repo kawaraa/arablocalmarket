ba2a242907462fb2172836f42b71031bb5088ebb
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-08-14 16:23:06 +0300

    Fixing home page videos don't auto play v-2.

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,9 +1,9 @@
 // self.importScripts('foo.js', 'bar.js');
 
 const staticFileCacheName =
-  "static-files-v-0njhvb83erwyuqwdbvujpo02i98rt4r79q0djoancbsfyq8e7t6534702q9oapoanf";
+  "static-files-v-0njhvb83erwyuqwdbvujpo02i98rt4r79q0djoancbsfyq8e7t6534702q9oapoan";
 // const filesMustCache = /(googleapis|gstatic)|\.(JS|CSS|SVG|PNG|JPG|jPEG|GIF|ICO|JSON)$/gim;
-const staticFileCachePaths = ["/", "/offline.html", "/barcode-scanner/quagga.min.js", "/signin", "/signup"];
+const staticFileCachePaths = ["/offline.html", "/barcode-scanner/quagga.min.js", "/signin", "/signup"];
 
 self.addEventListener("install", (evt) => {
   evt.waitUntil(caches.open(staticFileCacheName).then((cache) => cache.addAll(staticFileCachePaths)));
@@ -23,11 +23,13 @@ self.addEventListener("fetch", (evt) => evt.respondWith(handleRequest(evt.reques
 
 const handleRequest = async (request) => {
   const networkErrorResponse = Response.error();
-  console.log("Started Caching: >>> ", navigator.onLine, request.method, request.url);
   try {
-    // !request.url.includes("http")
-    // console.log("Started Caching: >>> ", navigator.onLine, request.method, request.url);
-    if (!["GET", "HEAD"].includes(request.method) || /api|api\/auth|api\/users/gim.test(request.url)) {
+    console.log("Started Caching: >>> ", navigator.onLine, request.method, request.url);
+    if (
+      !request.url.includes("http") ||
+      !["GET", "HEAD"].includes(request.method) ||
+      /api|api\/auth|api\/users/gim.test(request.url)
+    ) {
       return fetch(request);
     } else {
       const cachedResponse = await caches.match(request);
@@ -38,15 +40,17 @@ const handleRequest = async (request) => {
       }
 
       const response = await fetch(request);
-      if (!response.ok || response.type != "basic") return response;
+      if (!response.ok) return response;
 
-      await caches.open(staticFileCacheName).then((cache) => cache.put(request, response.clone()));
+      caches
+        .open(staticFileCacheName)
+        .then((cache) => cache.put(request, response.clone()).catch(() => null));
       return response;
     }
   } catch (error) {
     console.log("caches ERROR: >>>", request.method, request.url, error);
     if (request.method == "GET" && (request.mode == "navigate" || !request.url.includes("api"))) {
-      return caches.match(staticFileCachePaths[1]); // offline fallback page
+      return caches.match(staticFileCachePaths[0]); // offline fallback page
     }
     return networkErrorResponse;
   }
