64bf0500f13c23a5504c667d0bca9501240ff239
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-08-28 12:41:01 +0300

    Testing IOS install modal v-1.

--- frontend/app/test/page.js
+++ frontend/app/test/page.js
@@ -12,7 +12,7 @@ export default function Test() {
     const isIos = /iphone|ipad|ipod/gim.test(window.navigator.userAgent); // detect if the device is on iOS
     const isInstalled = "standalone" in window.navigator; // check if the device is in standalone mode
     // show the modal only once
-    return isIos && !isInstalled && !JSON.parse(localStorage.getItem("shownIosInstallModal"));
+    return isIos + " - " + !isInstalled + " - " + !JSON.parse(localStorage.getItem("shownIosInstallModal"));
   };
 
   return (
