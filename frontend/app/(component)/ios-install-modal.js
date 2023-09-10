b193c528ada7352aacc1ba2a5db3b62f2ff57655
Kawara <39919017+kawaraa@users.noreply.github.com>
2023-09-10 15:56:17 +0300

    Finished install modal v-1

--- frontend/app/(component)/ios-install-modal.js
+++ frontend/app/(component)/ios-install-modal.js
@@ -1,17 +1,13 @@
 "use client";
 import { useContext, useEffect, useState } from "react";
+import Image from "next/image";
 import { AppSessionContext } from "../app-session-context";
 import Modal from "./(styled)/modal";
-import Image from "next/image";
+import { Cookies } from "../(service)/utilities";
 
 export default function IosInstallModal() {
   const { lang } = useContext(AppSessionContext);
   const [open, setOpen] = useState(false);
-
-  const handleInstall = (e) => {
-    e.preventDefault();
-    if (navigator.share) return navigator.share({ title: "ALM", url: window.location.origin });
-  };
 
   const handleCancel = () => {
     localStorage.setItem("shownIosInstallModal", "true");
@@ -25,8 +21,8 @@ export default function IosInstallModal() {
   };
 
   useEffect(() => {
-    if (shouldShowIosInstallModal()) setTimeout(() => setOpen(true), 1000);
-  }, []);
+    if (Cookies.get("lang") && shouldShowIosInstallModal()) setTimeout(() => setOpen(true), 1000);
+  }, [lang]);
 
   return (
     <Modal lang={lang} title={content.title[lang]} open={open} onCancel={() => handleCancel(false)}>
@@ -38,9 +34,9 @@ export default function IosInstallModal() {
         {content.p[lang][0]}
       </p>
 
-      <p dir="auto" className="mt-5 flex justify-center items-center">
+      <p dir="auto" className="mt-5 flex justify-center items-center flex-wrap">
         {content.p[lang][1]}
-        <span onClick={handleInstall} className="pb-2 w-10 text-blue cursor-pointer">
+        <span className="pb-2 w-10 min-w-[40px] text-blue cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="-3.5 0 19 19" fill="currentColor">
             <path d="M11.182 6.771v9.364a.497.497 0 0 1-.496.495H1.314a.497.497 0 0 1-.496-.495V6.771a.497.497 0 0 1 .496-.495h3.259v1.108H1.927v8.138h8.146V7.384h-2.65V6.276h3.263a.497.497 0 0 1 .496.495zM8.91 4.97a.554.554 0 0 1-.783 0L6.552 3.394v7.36a.554.554 0 0 1-1.109 0v-7.36L3.87 4.97a.554.554 0 0 1-.784-.783l2.52-2.521a.554.554 0 0 1 .784 0l2.521 2.52a.554.554 0 0 1 0 .784z" />
           </svg>
