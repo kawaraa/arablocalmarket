50c0b2f332df4df67b074d4da44f21c77909e91e
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-05-28 18:05:23 +0200

    Fixing commands are not executed v1.

--- .github/backend-deploy-stript.sh
+++ .github/backend-deploy-stript.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-ls
 
 # Copy the sources code to the remote server
-scp -r ./backend/app.tar.gz root@$1:/root
+iac/retry-script.sh 3 scp -r app.tar.gz root@$1:/root
 
 # Run initialization script on the remote serve
-ssh root@$1 "~/iac/retry-script.sh init-setup"
+iac/retry-script.sh 3 ssh root@$1 "~/iac/retry-script.sh init-setup"
