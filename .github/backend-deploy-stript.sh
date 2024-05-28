5789faac19708f5c85d3b2b967161b4f9b6b89ec
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-05-28 18:21:23 +0200

    Fixing commands are not executed v2.

--- .github/backend-deploy-stript.sh
+++ .github/backend-deploy-stript.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo "(($1)) VM IP Address"
+sleep 30
 # Copy the sources code to the remote server
-iac/retry-script.sh 3 scp -r app.tar.gz root@$1:/root
+# iac/retry-script.sh 3 scp -r app.tar.gz root@$1:/root
+scp -r app.tar.gz root@$1:/root
 
-# Run initialization script on the remote serve
-iac/retry-script.sh 3 ssh root@$1 "~/iac/retry-script.sh init-setup"
+# # Run initialization script on the remote serve
+ssh root@$1 "~/iac/retry-script.sh init-setup"
