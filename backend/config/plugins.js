0f08fd1f78c646761c7d681164021cd75a2ff262
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-05-29 15:49:53 +0200

    Configuring Terraform state.

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
       // sizeLimit: 50 * 1024 * 1024, // 256mb in bytes, The default is 200MB
       provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
       providerOptions: {
-        baseUrl: env("GCP_BASE_URL") + env("GCP_BUCKET_NAME"),
+        baseUrl: `https://storage.googleapis.com/${env("GCP_BUCKET_NAME")}`,
         bucketName: env("GCP_BUCKET_NAME"),
         publicFiles: false,
         uniform: env("GCP_UNIFORM"),
--- backend/iac/main.tf
+++ backend/iac/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  # The state configurations
+  backend "gcs" {
+    bucket = "arablocalmarket-bucket"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -7,26 +13,59 @@ terraform {
   }
 }
 
+variable "alm_port" {
+  default = 1337
+}
 variable "digitalocean_token" {
   sensitive = true
 }
+
 provider "digitalocean" {
   token = var.digitalocean_token
 }
 
-resource "digitalocean_ssh_key" "web" {
+# This is already configured
+# resource "digitalocean_tag" "alm_db_tag" {
+#   name = "alm-db-tag"
+# }
+# resource "digitalocean_database_cluster" "alm_db" {
+#   name       = "alm-database"
+#   engine     = "mysql"
+#   version    = "8"
+#   size       = "db-s-1vcpu-1gb"
+#   region     = "fra1"
+#   node_count = 1
+# }
+# resource "digitalocean_database_firewall" "alm_db-fw" {
+#   cluster_id = digitalocean_database_cluster.alm_db.id
+
+#   rule {
+#     type = "droplet"
+#     # value = digitalocean_droplet.vm.id
+#     value = digitalocean_tag.alm_db_tag.id
+#   }
+# }
+
+resource "digitalocean_ssh_key" "auth" {
   name       = "VM SSH key"
   public_key = file("${path.module}/id_rsa.pub")
 }
 
-resource "digitalocean_droplet" "web" {
-  name   = "alm-backend-test"
+# Docs for Digitalocean Resources:
+# https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs
+resource "digitalocean_droplet" "vm" {
+  count = 1
+  name  = "alm-backend-${count.index}"
+  # name   = "alm-backend-test"
   region = "fra1"
   image  = "ubuntu-23-10-x64"
   size   = "s-1vcpu-1gb"
   # disk     = "25"
-  ssh_keys = [digitalocean_ssh_key.web.id]
-  tags     = ["api", "alm"]
+  # monitoring = true
+  # private_networking = true
+  ssh_keys = [digitalocean_ssh_key.auth.id]
+  tags     = ["api", "alm", digitalocean_tag.alm_db_tag.id]
+
 
   provisioner "remote-exec" {
     connection {
@@ -53,16 +92,108 @@ resource "digitalocean_droplet" "web" {
   }
 }
 
+# This is already configured
+# resource "digitalocean_certificate" "alm_cert" {
+#   name    = "alm-ssl-certificate"
+#   type    = "lets_encrypt"
+#   domains = ["api.arablocalmarket.com"]
+# }
+
+# # This is already configured
+# resource "digitalocean_loadbalancer" "public" {
+#   name   = "alm-loadbalancer"
+#   region = "fra1"
+
+#   forwarding_rule {
+#     entry_port     = 80
+#     entry_protocol = "http"
+
+#     target_port     = var.alm_port
+#     target_protocol = "http"
+#   }
+
+#   # if digitalocean_certificate is enabled
+#   forwarding_rule {
+#     entry_port     = 443
+#     entry_protocol = "https"
+
+#     target_port     = var.alm_port
+#     target_protocol = "http"
+
+#     certificate_name = digitalocean_certificate.alm_cert.name
+#   }
+
+#   healthcheck {
+#     port     = var.alm_port
+#     protocol = "http"
+#     path     = "/"
+#   }
+
+#   # if digitalocean_certificate is enabled
+#   # redirect_http_to_https = true
+#   # droplet_ids = [digitalocean_droplet.vm.id] # if it's one node
+#   droplet_ids = digitalocean_droplet.vm.*.id # if it's more then one node
+# }
+
+# ========== digitalocean droplet firewall ==========
+# Docs: https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/firewall
+# resource "digitalocean_firewall" "vm_fw" {
+#   name = "vms-firewall-rules-only-22-80-and-443"
+
+#   droplet_ids = digitalocean_droplet.vm.*.id
+
+#   inbound_rule {
+#     protocol         = "tcp"
+#     port_range       = "22"
+#     source_addresses = ["0.0.0.0/0", "::/0"] # allow anyone to ssh into vms
+#     # source_addresses = ["192.168.1.0/24", "2002:1:2::/48"] # Or restrict access
+#   }
+#   inbound_rule {
+#     protocol   = "tcp"
+#     port_range = "80"
+#     # source_addresses = ["0.0.0.0/0", "::/0"] # allow anyone to access vms via http
+#     source_load_balancer_uids = [digitalocean_loadbalancer.public.id] # allow only loadbalancer
+#   }
+#   inbound_rule {
+#     protocol   = "tcp"
+#     port_range = "443"
+#     # source_addresses = ["0.0.0.0/0", "::/0"]  # allow anyone to access vms via https
+#     source_load_balancer_uids = [digitalocean_loadbalancer.public.id] # allow only loadbalancer
+#   }
+#   inbound_rule {
+#     protocol         = "icmp"
+#     source_addresses = ["0.0.0.0/0", "::/0"]
+#   }
+
+#   outbound_rule {
+#     protocol              = "tcp"
+#     port_range            = "1-65535"
+#     destination_addresses = ["0.0.0.0/0", "::/0"]
+#   }
+#   outbound_rule {
+#     protocol              = "udp"
+#     port_range            = "1-65535"
+#     destination_addresses = ["0.0.0.0/0", "::/0"]
+#   }
+#   outbound_rule {
+#     protocol              = "icmp"
+#     destination_addresses = ["0.0.0.0/0", "::/0"]
+#   }
+# }
+
 resource "digitalocean_project" "alm_project" {
   name        = "ArabLocalMarket"
   description = "This projeect represents production resources for ArabLocalMarket App."
   purpose     = "Web Application"
   environment = "Production"
-  resources   = [digitalocean_droplet.web.urn]
+  # resources   = [digitalocean_droplet.vm.urn]
+  resources = [digitalocean_droplet.vm.*.urn, digitalocean_loadbalancer.public.urn]
+  # resources   = digitalocean_droplet.vm.*.urn # if it's more then one node
 }
 
 output "droplet_ip" {
-  value = digitalocean_droplet.web.ipv4_address
+  value = digitalocean_droplet.vm.ipv4_address
+  # value = digitalocean_loadbalancer.public.ip
 }
 
 
@@ -112,3 +243,5 @@ output "droplet_ip" {
 #   rm -f ~/.pm2/logs/*
 #   EOF
 # ]
+
+
